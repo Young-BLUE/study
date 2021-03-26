ffad7bddf0debc50c858f1310e14532c304519d6
Young-BLUE <youngblue.coding@gmail.com>
2021-03-26 16:59:40 +0900

    category user 테이블 테스트

--- src/main/java/com/example/study/repository/UserRepository.java
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -8,6 +8,19 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> { // GenericValue, PK 의 자료형
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     // query를 method로 사용한다는 의미에서 query method 라고도 함
     // select * from user where account = ? << test03, test04 등등
     Optional<User> findByAccount(String account);  // 변수명과는 상관 없이 매칭되지만 동일하게 맞추는 것을 추천
@@ -17,6 +30,8 @@ public interface UserRepository extends JpaRepository<User,Long> { // GenericVal
 
     // select * from user where account = ? and email = ?
     Optional<User> findByAccountAndEmail(String account, String email);
+
+     */
 
 }
 
