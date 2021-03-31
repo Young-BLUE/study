5b10b825a767b27297a003b5e4546385404d03e5
Young-BLUE <youngblue.coding@gmail.com>
2021-03-31 10:39:08 +0900

    8-2-03. 상품 API READ

--- src/main/java/com/example/study/service/ItemApiLogicService.java
+++ src/main/java/com/example/study/service/ItemApiLogicService.java
@@ -45,7 +45,13 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
 
     @Override
     public Header<ItemApiResponse> read(Long id) {
-        return null;
+
+        return itemRepository.findById(id)
+                .map(item -> response(item))
+                .orElseGet(() -> Header.ERROR("아이템 데이터 없음"));
+
+
+
     }
 
     @Override
